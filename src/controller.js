b0789ae19636b9057d95517eaa901586b04624c6
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-18 12:50:27 +0700

    feat(getRatingReview): Added get rating review services

--- src/controller.js
+++ src/controller.js
@@ -6,3 +6,19 @@ let connection = require('./connection');
 exports.index = function (req, res) {
 	response.ok("Transactions web services", res)
 };
+
+exports.getRatingReview = function (req, res) {
+	let movie_id = req.params.movie_id;
+
+	connection.query(
+    "SELECT * FROM transactions_history WHERE movieID = ? AND status = 'Success'",
+    [movie_id],
+    function(error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
+};
