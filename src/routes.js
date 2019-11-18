7504aaa56abe59998145e9b45646692860b89dff
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-18 12:53:30 +0700

    feat(getSeat): Added get seat endpoint

--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,7 @@ module.exports = function (app) {
 
 	app.route('/web_service_transactions/movie_id/:movie_id')
 		.get(todoList.getRatingReview);
+
+	app.route('/web_service_transactions/movie_id/:movie_id/date/:date/time/:time')
+		.get(todoList.getSeat);
 };
