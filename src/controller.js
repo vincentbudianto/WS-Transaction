904e8753ba530c9ad4ed66fb7b29489e3f10ee91
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-18 12:56:27 +0700

    feat(addTransaction): Added add transaction services

--- src/controller.js
+++ src/controller.js
@@ -22,3 +22,23 @@ exports.getRatingReview = function (req, res) {
     }
   );
 };
+
+exports.addTransaction = function (req, res) {
+	let user_id = req.body.user_id;
+	let account_number = req.body.account_number;
+	let virtual_number = req.body.virtual_number;
+	let movie_id = req.body.movie_id;
+	let date = req.body.date;
+	let time = req.body.time;
+	let seat = req.body.seat;
+	let price = req.body.price;
+	let status = req.body.status;
+
+	connection.query('INSERT INTO transactions_history (userID, accountNumber, virtualNumber, movieID, historyDate, historyTime, seatNumber, price, status) values (?,?,?,?,?,?,?,?,?)', [user_id, account_number, virtual_number, movie_id, date, time, seat, price, status], function (error, rows, fields) {
+		if (error) {
+			console.log(error)
+		} else {
+			response.ok(rows, res)
+		}
+	});
+};
