ae61bc5c965796bbcf3b4ca3ad69f7bf8b07190d
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-18 12:53:21 +0700

    feat(getSeat): Added get seat services

--- src/controller.js
+++ src/controller.js
@@ -22,3 +22,17 @@ exports.getRatingReview = function (req, res) {
     }
   );
 };
+
+exports.getSeat = function (req, res) {
+	let movie_id = req.params.movie_id;
+	let date = req.params.date;
+	let time = req.params.time;
+
+	connection.query('SELECT seatNumber FROM transactions_history WHERE movieID = ? and historyDate = ? and historyTime = ? and (status = "Pending" or status = "Success")', [movie_id, date, time], function (error, rows, fields) {
+		if (error) {
+			console.log(error)
+		} else {
+			response.ok(rows, res)
+		}
+	});
+};
