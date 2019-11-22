4550a2997a7b1ba9eab996d49e5dfef6fdda4ba7
vincent VB <vincent.budianto@yahoo.co.id>
2019-11-22 18:07:51 +0700

    ci(WS-Transactions): Fixed build script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd ws-transactions
 npm install
 pm2 delete --silent web_service_transaksi || ':'
-pm2 start npm run start --name web_service_transaksi
+pm2 start src/server.js --name web_service_transaksi
 pm2 status
